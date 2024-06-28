b0c845c8e180e2243d152270b01392bac0bf5fd3
Christopher Zanoli <christopherzanoli2000@gmail.com>
2024-06-28 16:58:00 +0200

    added pre-processing for test-set, run all classifiers

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import yaml
 from pathlib import Path
 #from ydata_profiling import ProfileReport
 import re
@@ -20,11 +19,6 @@ from symspellpy import SymSpell
 import pkg_resources
 from tqdm import tqdm
 
-# Load configuration from config.yaml
-with open(Path(__file__).resolve().parent/'config.yml', 'r') as file:
-    config = yaml.safe_load(file)
-train_files = config['raw_train_paths']
-test_file = config['raw_test_path']
 nltk.download('stopwords')
 nltk.download('words')
 common = words.words()
@@ -36,8 +30,7 @@ tqdm.pandas()
 
 class DataProcessor:
     def __init__(self, duplicates_policy, shared_duplicates_policy, conflict_policy, 
-                 dataset_type, prj_dir, train_files, test_file, output_file, preprocessing_policy,
-                 hashtag_policy):
+                 dataset_type, prj_dir, train_files, test_file, preprocessing_policy, hashtag_policy):
         self.duplicates_policy = duplicates_policy
         self.shared_duplicates_policy = shared_duplicates_policy
         self.conflict_policy = conflict_policy
@@ -45,23 +38,30 @@ class DataProcessor:
         self.prj_dir = prj_dir
         self.train_files = train_files
         self.test_file = test_file
-        self.output_file = output_file
         self.preprocessing_policy = preprocessing_policy
         self.hashtag_policy = hashtag_policy
     
-    def load_training_data(self):
-        if self.dataset_type not in self.train_files.keys():
-            raise ValueError(f"Invalid training dataset_type. Expected one of {list(self.train_files.keys())}")
-        df = pd.DataFrame()
-        for file in self.train_files[self.dataset_type]:
-            label = "positive" if "pos" in file else "negative" if "neg" in file else None
-            with open(file, 'r', encoding='utf-8') as file:
-                tweets = file.readlines()
-            tmp_df = pd.DataFrame(tweets, columns=["tweet"])
-            tmp_df["label"] = label
-            df = pd.concat([df, tmp_df], ignore_index=True)
-        return df
+    def load_data(self, is_test=False):
+        if is_test:
+            df = pd.DataFrame()
+            with open(self.test_file, 'r', encoding='utf-8') as file:
+                data = [line.split(',', 1) for line in file.readlines()]
+            df = pd.DataFrame(data, columns=["id", "tweet"])
+            return df
+        else:
+            if self.dataset_type not in self.train_files.keys():
+                raise ValueError(f"Invalid training dataset_type. Expected one of {list(self.train_files.keys())}")
+            df = pd.DataFrame()
+            for file in self.train_files[self.dataset_type]:
+                label = "positive" if "pos" in file else "negative" if "neg" in file else None
+                with open(file, 'r', encoding='utf-8') as file:
+                    tweets = file.readlines()
+                tmp_df = pd.DataFrame(tweets, columns=["tweet"])
+                tmp_df["label"] = label
+                df = pd.concat([df, tmp_df], ignore_index=True)
+            return df
 
+    '''
     def load_test_data(self, dataset_type="test"):
         if dataset_type not in test_file.keys():
             raise ValueError(f"Invalid test dataset_type. Expected {list(train_files.keys())}")
@@ -70,9 +70,10 @@ class DataProcessor:
             data = [line.split(',', 1) for line in file.readlines()]
         df = pd.DataFrame(data, columns=["id", "tweet"])
         return df
+    '''
     
-    def save_df_to_csv(self, df):
-        df.to_csv(self.output_file, index=False)
+    def save_df_to_csv(self, df, output_file):
+        df.to_csv(output_file, index=False)
         
     def nulls_info(self, df):
         return df.isnull().sum()
@@ -81,7 +82,7 @@ class DataProcessor:
         #profile = ProfileReport(df, title="Twitter Sentiment EDA Report", minimal=True)
         #profile.to_file(self.prj_dir / f"reports/twitter_sentiment_eda_{self.dataset_type}.html")
     
-    def process_dataframe(self, df, df2=None):
+    def process_dataframe(self, df):
         """
         Process the DataFrame based on the specified policies.
         
