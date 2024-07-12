55121d0e2213f16a068c26a9a1d11b4ae193df03
Matteo Boglioni <ilboglions@Matteos-MacBook-Pro.local>
2024-07-12 17:54:17 +0200

    update train_llms

--- src/models/train_llms.py
+++ src/models/train_llms.py
@@ -8,23 +8,26 @@ import time
 from tqdm.auto import tqdm
 import numpy as np
 from utils import *
+import time
 
 
 
 def train(train_path, model, tokenizer, lr = 2e-5,num_epochs=3, seed=42, validation = False):
-
+    
     set_seed(seed)
     df = pd.read_csv(train_path)
     df['label'] = df['label'].map({"positive": 1, "negative": 0}) 
     if validation:
-        train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
+        train_df, val_df = train_test_split(df, test_size=0.1, random_state=seed)
         val_dataset = TweetDataset(tweets=val_df['tweet'].tolist(), labels=val_df['label'].tolist(), tokenizer=tokenizer)
-        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False,pin_memory=True)
+        del val_dataset
     else:
         train_df = df
-    
     train_dataset = TweetDataset(tweets=train_df['tweet'].tolist(), labels=train_df['label'].tolist(), tokenizer=tokenizer)
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)  
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,pin_memory=True)  
+    del train_dataset
+    del df
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
     print("Using ", device)
@@ -35,7 +38,6 @@ def train(train_path, model, tokenizer, lr = 2e-5,num_epochs=3, seed=42, validat
         name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
     )
     progress_bar = tqdm(range(num_training_steps))
-
     model.train()
     for epoch in range(num_epochs):
         start_time = time.time()
@@ -53,11 +55,11 @@ def train(train_path, model, tokenizer, lr = 2e-5,num_epochs=3, seed=42, validat
             all_labels = []
             all_preds = []
             with torch.no_grad():
-                for batch in val_loader:
-                    batch = {k: v.to(device) for k, v in batch.items()}
-                    outputs = model(**batch)
+                for eval_batch in val_loader:
+                    eval_batch = {k: v.to(device) for k, v in eval_batch.items()}
+                    outputs = model(**eval_batch)
                     preds = torch.where(outputs.logits <= 0.5, torch.tensor(0), torch.tensor(1))
-                    all_labels.extend(batch['labels'].cpu().numpy())
+                    all_labels.extend(eval_batch['labels'].cpu().numpy())
                     all_preds.extend(preds.cpu().numpy())
             
             val_accuracy = accuracy_score(all_labels, all_preds)
